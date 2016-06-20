4cd4cd2a52fd700faa85c274c10e7bbd61ecde8e
Andrew54757 <andrews54757@gmail.com>
2016-06-20 13:38:33 -0400

    Put copy notice on certain files (#1093)
    * Update Multiverse.js
    
    * Create kr.js
    
    * Update Multiverse.js
    
    * Update ConfigService.js
    
    * Update ConsoleService.js
    
    * Update ControlServer.js
    
    * Update GameServer.js
    
    * Update GeneratorService.js
    
    * Update PacketHandler.js
    
    * Update Physics.js
    
    * Update PlayerTracker.js
    
    * Update PluginLoader.js
    
    * Update Updater.js
    
    * Update WorldModel.js
    
    * Update op.js
    
    * Update utilities.js
    
    * Update es.js
    
    * Update CommandList.js
    
    * Update BotPlayer.js
    
    * Update BotLoader.js
    
    * Update MinionLoader.js
    
    * Update MinionPlayer.js
    
    * Update ClientPacket.js
    
    * Update DataPacket.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -1,3 +1,17 @@
+/*
+Proverbs 20:18:
+   Bread obtained by falsehood is sweet to a man, But afterward his mouth will be filled with gravel.
+
+We worked really hard for this project. Although we dont care if you enhance it and publish, we would care
+if you copy it and claim our work as your own. Although it might feel good, to take the credit, you would ultimatly
+regret it. But please feel free to change the files and publish putting your name up as well as ours.
+We will also not get into legalities. but please dont take advantage that we dont use
+legalities. Instead, treat us with respect like we treat you. 
+
+Sincerely
+The AJS Dev Team.
+
+*/
 // Imports
 //var Teams = require('../gamemodes/Teams.js');
 //var GameMode = require('../gamemodes');
