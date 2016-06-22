873de88d1f919f64101a6eca561157eb95036d3e
Andrew54757 <andrews54757@gmail.com>
2016-06-22 11:31:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const Readline = require('readline');
-const VERSION = '17.3.1';
+const VERSION = '17.3.5';
 const Multiverse = require('./core/Multiverse');
 let multiverse = new Multiverse(VERSION);
 //throw error
