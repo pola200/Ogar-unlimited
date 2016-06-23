2d652816d022ca9243719d66134beea36b4e56ec
Andrew54757 <andrews54757@gmail.com>
2016-06-23 17:19:24 -0400

    Update colorchat.js

--- src/modules/commands/colorchat.js
+++ src/modules/commands/colorchat.js
@@ -1,1 +1,9 @@
 module.exports = function (gameServer,player, split) {
+  var msg = function (m) {
+    gameServer.pm(player.pID,m);
+  }
+  if (!split[1]) {
+    msg("Please Put a valid ")
+  }
+  
+}
