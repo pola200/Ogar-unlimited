3a2111b8cfc035c3a0f7cee5f32be0a428a237fb
Andrew54757 <andrews54757@gmail.com>
2016-06-23 17:13:18 -0400

    Update Chat.js

--- src/packet/Chat.js
+++ src/packet/Chat.js
@@ -24,8 +24,8 @@ Chat.prototype.build = function () {
     var buf = new ArrayBuffer(9 + 2 * nick.length + 2 * this.message.length);
     var view = new DataView(buf);
     var color = {'r': 155, 'g': 155, 'b': 155};
-    if (this.sender.cells && this.sender.cells.length > 0 && !this.sender.chatcolor) {
-        color = this.sender.cells[0].getColor();
+    if (this.sender.cells && this.sender.cells.length > 0) {
+        color = this.sender.chatColor;
     }
     view.setUint8(0, 99);
     view.setUint8(1, 0); // flags for client; for future use
