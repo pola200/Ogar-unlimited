2d6a6a7b727da44f2598c4bd38c528e5d075ff82
Andrew54757 <andrews54757@gmail.com>
2016-06-24 16:20:38 -0400

    Update pm.js

--- src/modules/commands/pm.js
+++ src/modules/commands/pm.js
@@ -1,5 +1,20 @@
 module.exports = function(gameServer,player,split) {
-
-
+var msg = function (m) {
+    gameServer.pm(player.pID,m);
+  }
+  if (!split[1]) {
+    msg("Please specify a chatname");
+    return;
+  }
+  var message = split.slice(2, split.length).join(' ');
+  for (var i in gameServer.clients) {
+    var client = gameServer.clients[i].playerTracker;
+    if (client && client.chatName && client.chatName == split[1]) {
+      gameServer.pm(client.pID,message,"[" + player.chatname + " >> " + client.chatname + "]");
+      gameServer.pm(player.pID,message,"[" + player.chatname + " >> " + client.chatname + "]");
+      return;
+    }
+  }
+msg("Invalid Chatname")
 
 }
